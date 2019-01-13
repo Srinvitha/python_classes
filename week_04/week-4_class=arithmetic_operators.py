18834e47b8f1c90d8cc8464c48d102542e187f9a
Srinvitha <srinvitha@icloud.com>
2019-01-13 18:03:39 +0530

    week_04-commit

---
+++ week_04/week-4_class=arithmetic_operators.py
@@ -0,0 +1,31 @@
+a = 2
+print(a)
+b = 4
+print(b)
+print("\n")                       # next line
+
+c = a+b                           # Addition
+print(c)
+
+d = b-a                           # Subtraction
+print(d)
+
+e = a*b                           # Multiplication
+print(e)
+
+f = b/a                           # Division
+print(f)
+
+g = b%a                           # Modulus
+print(g)
+
+h = a**b                          # Exponent
+print(h)
+
+print ("\n")
+
+i = 23
+p = 45
+t = i**p
+print (t)
+
