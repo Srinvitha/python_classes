d58c2b5c03e6d96750da0c0ef32c416ec1f76385
Srinvitha <srinvitha@icloud.com>
2019-01-18 17:32:16 +0530

    Week_04

---
+++ week_04/week-04_homework.py
@@ -0,0 +1,11 @@
+n =  input (" Enter any number of your choice => \n")
+print("\n")
+
+if n%2 == 0 :
+    print ("This number is divisible by 2.")
+
+if n%6 == 0 :
+    print ("And this number is divisible by 6.")
+
+else:
+    print ("This number is not divisible by 2 nor by 6 .")
