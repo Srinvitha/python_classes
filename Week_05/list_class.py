8e29a37aca3ec28e81b165934885e0d915285986
Srinvitha <srinvitha@icloud.com>
2019-01-20 17:54:00 +0530

    class commit

---
+++ Week_05/list_class.py
@@ -0,0 +1,23 @@
+hero_list= ["Superman", "Spiderman", "Hulk", "Thor", "Batman", "Wonder Woman", "Captain Marvel", "Miss Marvel", "Black Widow"]
+                                                        # concept- List
+print (hero_list[0])
+print (hero_list[1])
+print (hero_list[2])
+print (hero_list[3])
+print (hero_list[4])
+print ("\n")
+print (hero_list[5])
+print (hero_list[6])
+print (hero_list[7])
+print (hero_list[8])
+
+winx_list = ["Bloom", "Flora", "Tecna", "Musa", "Aisha", "Stella"]
+
+print ("\n")
+print (winx_list[0])
+print (winx_list[1])
+print (winx_list[2])
+print (winx_list[3])
+print (winx_list[4])
+print (winx_list[5])
+
