267f8aa5edb22b4d07e8504b330673f55a62c4e3
Srinvitha <srinvitha@icloud.com>
2019-01-20 16:21:41 +0530

    class commit

---
+++ week_04/week-4_class=comparison_operators.py
@@ -0,0 +1,31 @@
+# Comparison Operators, Desicion Making
+z = 10
+y = 3
+
+# Using Comparison operators to compare "z" and "y" ...
+print(z==y)
+
+print(z!=y)
+
+print(z>y)
+
+print(z<y)
+
+print(z>=y)
+
+print(z<=y)
+
+# Desicion making
+
+print ("\n")
+if z>y :
+    print ("z is greater than y")
+else :
+    print ("y is greater than z")
+    print ("\n")
+
+a = int(input("please type in a number(I wish, less than 10)..."))
+if a<z :
+    print ("You chose a number less than 10...:)")
+else :
+    print ("You didn't choose a number less than 10.:( ")
