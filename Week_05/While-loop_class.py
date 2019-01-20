49646df74c6596bab2d3c694f3c06de4982f09ff
Srinvitha <srinvitha@icloud.com>
2019-01-20 17:54:29 +0530

    class commit

---
+++ Week_05/While-loop_class.py
@@ -0,0 +1,25 @@
+# concept - Loops => 1.While loop
+
+count=0
+while count <= 100:                                                                 # Condition
+    if count%10==0 :
+        print (count)
+    count = count + 1
+
+print ("\n")
+
+count=0
+while count <= 100:                                                                 # Condition
+    if count%20==2 :
+        print (count)
+    count = count + 1
+else:
+    print ("not divided")
+
+
+k = (int(input("Give a number :")))
+if k==100:
+    print("(: A Century!!...")
+
+else:
+    print ("Not a century :(.")
