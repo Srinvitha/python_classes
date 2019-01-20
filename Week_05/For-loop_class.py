48c87e0e274ec66c91bddc312116967c436d3b97
Srinvitha <srinvitha@icloud.com>
2019-01-20 17:53:47 +0530

    class commit

---
+++ Week_05/For-loop_class.py
@@ -0,0 +1,19 @@
+# concept - Loops => 2.For loop
+
+super_girls=["Wonder Woman", "Captain Marvel", "Miss Marvel", "Black Widow"]
+print (len(super_girls))
+for best in super_girls:
+    if best == "Miss Marvel":
+         print ( best)
+
+num_list=[2,42,14.3,21,50]
+
+for number in num_list:
+    if number==50:
+        print(number)
+
+n_list=[2,3,4,5,1,6,7,8,9,13,42]
+
+for num in n_list:
+    if num%2==0:
+        print (num)
