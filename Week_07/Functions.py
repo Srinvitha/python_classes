37e192efb6a8b8a7152bb3b6582e1f0867e99d0b
Srinvitha <srinvitha@icloud.com>
2019-02-03 18:02:24 +0530

    Class commit

---
+++ Week_07/Functions.py
@@ -0,0 +1,45 @@
+def string_to_uppercase(s):
+    y = s.upper()
+    return y
+
+
+fruits = ["Mango", "Litchi", "Strawberry", "Pineapple", "Custard-apple", "Apple", "Banana"]
+
+for f in fruits:
+    print(string_to_uppercase(f))
+
+output = string_to_uppercase("I am a Girl.")
+print(output)
+
+def string_to_capitalize(c):
+    z = c.capitalize()
+    return z
+
+winx_list = ["Bloom", "Flora", "Tecna", "Musa", "Aisha", "Stella"]
+
+for w in winx_list:
+    print(string_to_capitalize(w))
+
+
+def string_to_lowercase(l):
+    x = l.lower()
+    return x
+
+
+villains = ["Thanos", "Joker", "Vyan", "Venom", "Voldemort"]
+
+for v in villains:
+    print(string_to_lowercase(v))
+
+
+
+def replace(r):
+    p = r.replace("a", "2")
+    return p
+
+villains = ["Thanos", "Joker", "Vyan", "Venom", "Voldemort"]
+
+for v in villains:
+    print(replace(v))
+
+
