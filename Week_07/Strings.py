b2ebf576e410e672c9be9bdbf5d7ea9b2236cb43
Srinvitha <srinvitha@icloud.com>
2019-02-03 18:02:53 +0530

    Class commit

---
+++ Week_07/Strings.py
@@ -0,0 +1,13 @@
+# Strings are by default , lists.
+# Strings can be enclosed by either single or double quotes.
+
+
+hello = "Hello Python world !!! "
+print(hello[6])
+print(hello[-6])
+
+
+# Can be accessed via indexes
+python_string = 'Python Programming'
+print(python_string[0:6])
+
