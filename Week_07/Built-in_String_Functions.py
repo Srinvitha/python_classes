8bc83a676016a5008cae3ef3e2a070c0720e6e38
Srinvitha <srinvitha@icloud.com>
2019-02-03 18:02:07 +0530

    Class commit

---
+++ Week_07/Built-in_String_Functions.py
@@ -0,0 +1,21 @@
+s = " I am Batgirl ..."
+print(s)
+
+print(s.lower())
+print(s.upper())
+print(s.capitalize())
+
+alph = "qwerty42860"
+print(alph)
+print(alph.isalpha())
+print(alph.isalnum())
+print(alph.replace("qwerty42860", "22222222222244444444"))
+
+n = "1234567890"
+print(n.isnumeric)
+
+alp = "abcdefg6"
+alp2 = alp.replace("abcdefg", "s")
+print (alp2)
+final = alp2.replace("6", "4242")
+print(final)
