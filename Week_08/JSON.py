88bedd29857d93342ea436d82faf4e246de11d35
Srinvitha <srinvitha@icloud.com>
2019-02-10 18:07:17 +0530

    Class commit

---
+++ Week_08/JSON.py
@@ -0,0 +1,15 @@
+# JSON = "JavaScript Object Notation"
+
+import json
+
+x = '{"Name":"John", "Age":24, "City": "New York"}'
+print(type(x))
+
+y = json.loads(x)
+print(type(y))        # The result is a Python Dictionary...
+
+print(y["Age"])
+
+
+
+
