3f973c0bf7e5542936ff2b58cf07a3801dbabb77
Srinvitha <srinvitha@icloud.com>
2019-02-10 18:07:53 +0530

    Class commit

---
+++ Week_08/Tuple.py
@@ -0,0 +1,15 @@
+# Tuples = Lists ; but in tupe it can't be changed like how we can in lists... It is a sequence of Immutable Python objects.
+# Tuples use Parentheses (or comma), whereas lists use square-brackets.
+
+
+
+num_list=[1,3,6,8,4]
+print(num_list)
+num_list[1] = 2
+print(num_list)
+print (len(num_list))
+
+num_tuple= 1,3,6,8,4
+print(num_tuple)
+num_list[1] = 2
+print(num_tuple)
