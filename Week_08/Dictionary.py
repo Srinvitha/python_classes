42d5d558e606b34549482c3404ab1429a9a199d2
Srinvitha <srinvitha@icloud.com>
2019-02-10 18:07:02 +0530

    Class commit

---
+++ Week_08/Dictionary.py
@@ -0,0 +1,31 @@
+# Dictionary is a set of key-value pairs.
+# It is enclosed in curly braces (or flower brackets).
+
+a = {1:'number one', 22 : 'number twenty-two'}     # Here 1 and 22 are keys and 'number one' and 'number twenty-two' are their values respectively.
+print(a)
+print(a[1])
+print(a[22])
+
+dict_employee = {"Employee_name": "Ravi", "Designation":"Vice President", "Age" : "34"}
+print (dict_employee)
+
+
+# Updating the dictionary.
+dict_employee ["Employee_name"] = "Ravi Chandra"
+dict_employee ["Designation"] = "CEO"
+print (dict_employee)
+
+del dict_employee["Age"]           #  comment this if you want to see clear work and then uncomment the clear.
+print(dict_employee)
+
+# dict_employee.clear()
+# print(dict_employee)
+print(dict_employee)
+
+
+dict_employee.update({"Hometown":"Hyderabad"})
+print(dict_employee)
+
+print (dict_employee.keys())
+print (dict_employee.values())
+
