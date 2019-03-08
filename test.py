58d4e3018dd4fbe8c1962ee5b844c6c19632196e
Srinvitha <srinvitha@icloud.com>
2019-03-08 18:00:59 +0530

    Home commit

---
+++ test.py
@@ -0,0 +1,47 @@
+import random
+import pickle
+
+class Bot:
+    current = ""
+    botText = "BOT> "
+
+    def __init__(self, data, saveFile):
+        self.data = data
+        self.saveFile = saveFile
+
+    def say(self, text):
+        print(self.botText + text)
+        self.current = text
+
+    def addResponse(self, userInput, response):
+        if userInput in self.data:
+            self.data[userInput].extend(response)
+
+        else:
+            self.data[userInput] = []
+            self.data[userInput].extend(response)
+
+
+    def evaluate(self, text):
+        if text in self.data:
+            self.say(random.choice(self.data[text]))
+
+        elif text == "/SAVE":
+            f = open(self.saveFile, 'wb')
+            pickle.dump(self.data, f)
+            f.close()
+
+        elif text == "/LOAD":
+            f = open(self.saveFile, 'rb')
+            self.data = pickle.load(f)
+            f.close()
+
+        elif text == "/DATA":
+            print(self.data)
+
+        else:
+            if not self.current in self.data:
+                self.data[self.current] = []
+
+            self.data[self.current].append(text)
+            self.say(text)
